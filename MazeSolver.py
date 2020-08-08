d92fab9dd9021006098676bf0aaead2273e22e32
Ahmed Nazir <31586040+ahmed-nazir@users.noreply.github.com>
2020-08-08 18:42:10 -0400

    Update MazeSolver.py
    Expanded maze to support bigger sizes

--- MazeSolver.py
+++ MazeSolver.py
@@ -3,28 +3,52 @@ import sys
 
 im = Image.new('RGB', (1000, 1000), (255, 255, 255))
 draw = ImageDraw.Draw(im)
-maze = [[0,0,0,0,0,0,8,0,0,0],[0,1,1,1,1,1,1,0,1,0],[0,1,0,0,0,0,0,0,1,0],[0,1,0,1,1,1,1,1,1,0],[0,1,0,1,0,0,0,0,0,0],[0,1,1,1,1,1,1,1,1,0],[0,1,0,0,0,0,1,0,0,0],[0,1,0,1,1,1,1,1,1,0],[0,1,0,0,1,0,1,0,1,0],[0,0,0,0,9,0,0,0,0,0]]
-for i in range(10):
-    for j in range(10):
+size = 50
+unsolved = True
+maze = [[0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0],
+        [0,1,0,0,0,0,0,0,1,0,1,0,0,1,0,0,1,0,1,0],
+        [0,1,0,1,1,1,1,1,1,0,1,0,0,0,0,0,1,0,1,0],
+        [0,1,0,1,0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,0],
+        [0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,0,0],
+        [0,1,0,0,0,0,1,0,0,0,1,0,1,0,1,0,1,1,1,0],
+        [0,1,0,1,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1,0],
+        [0,1,0,0,1,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0],
+        [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,0],
+        [0,1,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1,0],
+        [0,1,0,0,0,0,0,0,1,0,0,0,1,0,1,1,1,0,1,0],
+        [0,1,0,1,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,0],
+        [0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,0],
+        [0,1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,0],
+        [0,1,0,0,0,0,1,0,1,1,1,1,1,1,1,1,0,1,1,0],
+        [0,1,0,1,1,1,1,1,1,0,0,0,0,0,0,1,0,0,1,0],
+        [0,1,0,0,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,0]]
+
+for i in range(20):
+    for j in range(20):
         if maze[i][j] == 0:
-            draw.rectangle((0+(j*100),0+(i*100),100+(j*100),100+(i*100)), fill=(0,0,0), outline=(0,0,0))
+            draw.rectangle((0+(j*size),0+(i*size),size+(j*size),size+(i*size)), fill=(0,0,0), outline=(0,0,0))
         elif maze[i][j] == 1:
-            draw.rectangle((0+(j*100),0+(i*100),100+(j*100),100+(i*100)), fill=(255,255,255), outline=(0,0,0))
+            draw.rectangle((0+(j*size),0+(i*size),size+(j*size),size+(i*size)), fill=(255,255,255), outline=(0,0,0))
         elif maze[i][j] == 8:
-            draw.rectangle((0+(j*100),0+(i*100),100+(j*100),100+(i*100)), fill=(34,255,0), outline=(34,255,0))
+            draw.rectangle((0+(j*size),0+(i*size),size+(j*size),size+(i*size)), fill=(34,255,0), outline=(34,255,0))
             startY = i
             startX = j
             sY = i
             sX = j
         elif maze[i][j] == 9:
-            draw.rectangle((0+(j*100),0+(i*100),100+(j*100),100+(i*100)), fill=(255,0,0), outline=(255,0,0))
+            draw.rectangle((0+(j*size),0+(i*size),size+(j*size),size+(i*size)), fill=(255,0,0), outline=(255,0,0))
             endi = i
             endj = j
 im.save('Documents/Before.jpg', quality=100)
+
 traversed = [(startY,startX)]
 node = []
 completenode = []
 path = [(startY,startX)]
+print(traversed)
 def moveLeft():
     if startX>0:
         if (maze[startY][startX-1] != 0) and ((startY,startX-1) not in traversed):
@@ -32,7 +56,7 @@ def moveLeft():
         else:
             return False
 def moveRight():
-    if startX<10:
+    if startX<20:
         if (maze[startY][startX+1] != 0) and ((startY,startX+1) not in traversed):
             return True
         else:
@@ -44,16 +68,17 @@ def moveUp():
         else:
             return False
 def moveDown():
-    if startY<10:
+    if startY<20:
         if (maze[startY+1][startX] != 0) and ((startY+1,startX) not in traversed):
             return True
         else:
             return False
 
-for q in range(50):
+while unsolved:
     if startY == endi and startX == endj:
         print("Finished")
-        draw.rectangle((0+(startX*100),0+(startY*100),100+(startX*100),100+(startY*100)), fill=(255,0,0), outline=(255,0,0))
+        draw.rectangle((0+(startX*size),0+(startY*size),size+(startX*size),size+(startY*size)), fill=(255,0,0), outline=(255,0,0))
+        unsolved = False
         break
 
     if (not(moveDown() or moveLeft() or moveRight() or moveUp())):
@@ -66,7 +91,6 @@ for q in range(50):
         
     if (moveDown() and (moveLeft() or moveRight() or moveUp())) or (moveUp() and (moveLeft() or moveRight())) or(moveLeft() and moveRight()) :
         node.append((startY,startX))
-        completenode.append((startY,startX))
 
     if moveDown():
         startY = startY+1
@@ -90,17 +114,17 @@ for q in range(50):
 
 def PATH():
     for i in path:
-        draw.rectangle((0+(i[1]*100),0+(i[0]*100),100+(i[1]*100),100+(i[0]*100)), fill=(66,75,200), outline=(0,0,0))
-    draw.rectangle((0+(endj*100),0+(endi*100),100+(endj*100),100+(endi*100)), fill=(255,0,0), outline=(255,0,0))
-    draw.rectangle((0+(sX*100),0+(sY*100),100+(sX*100),100+(sY*100)), fill=(34,255,0), outline=(34,255,0))
+        draw.rectangle((0+(i[1]*size),0+(i[0]*size),size+(i[1]*size),size+(i[0]*size)), fill=(66,75,200), outline=(0,0,0))
+    draw.rectangle((0+(endj*size),0+(endi*size),size+(endj*size),size+(endi*size)), fill=(255,0,0), outline=(255,0,0))
+    draw.rectangle((0+(sX*size),0+(sY*size),size+(sX*size),size+(sY*size)), fill=(34,255,0), outline=(34,255,0))
 
 def TRAVERSED():
     for i in traversed:
-        draw.rectangle((0+(i[1]*100),0+(i[0]*100),100+(i[1]*100),100+(i[0]*100)), fill=(247,250,185), outline=(0,0,0))
-    draw.rectangle((0+(endj*100),0+(endi*100),100+(endj*100),100+(endi*100)), fill=(255,0,0), outline=(255,0,0))
-    draw.rectangle((0+(sX*100),0+(sY*100),100+(sX*100),100+(sY*100)), fill=(34,255,0), outline=(34,255,0))
+        draw.rectangle((0+(i[1]*size),0+(i[0]*size),size+(i[1]*size),size+(i[0]*size)), fill=(247,250,185), outline=(0,0,0))
+    draw.rectangle((0+(endj*size),0+(endi*size),size+(endj*size),size+(endi*size)), fill=(255,0,0), outline=(255,0,0))
+    draw.rectangle((0+(sX*size),0+(sY*size),size+(sX*size),size+(sY*size)), fill=(34,255,0), outline=(34,255,0))
 
-TRAVERSED()
+#TRAVERSED()
 PATH()
 
 im.save('Documents/Finished.jpg', quality=100)
